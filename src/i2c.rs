bced9587054de2defcaecc3f4fa8c61e574ceac9
jacksonn97 <jackson.n.97@proton.me>
2024-01-29 21:37:07 +0300

    Added spaces between code and their docs
    Fixed warnings about unused Result values in spi.rs

--- src/i2c.rs
+++ src/i2c.rs
@@ -5,6 +5,7 @@ use crate::*;
 /// Struct stores calibration data and settings from the sensor
 ///
 /// Have owns over SS
+
 pub struct BME280 {
     address: u8,
     calibrationdata: CalibrationData,
@@ -12,6 +13,7 @@ pub struct BME280 {
 }
 
 impl BME280 {
+
     /// Function gets tuple with i2c bus and address
     ///
     /// Then get calibration data and store settings in sensor
@@ -19,6 +21,7 @@ impl BME280 {
     ///:```rust
     /// let mut bme280 = BME280::init(&mut i2c, 0x76); // specify your i2c bus and address of sensor
     /// ```
+
     #[inline]
     pub fn init(i2c: &mut hal_I2c, address: u8) -> Self {
         let pt_data = Self::read_calibration_data_pt(address, i2c);
@@ -39,6 +42,7 @@ impl BME280 {
     /// ```rust
     /// let measure = bme280.get_measures(&mut spi);
     /// ```
+
     pub fn get_measures(&mut self, i2c: &mut hal_I2c) -> Measure {
         match self.settings.ctrl_meas & 0b00000011 {
             0b00000000 => (),
@@ -84,12 +88,14 @@ impl BME280 {
     /// Set you own settings for the sensor
     ///
     /// Read the datasheet if you want to do
+
     pub fn update_settings(&mut self, i2c: &mut hal_I2c, settings: Settings) {
         self.settings = settings;
         self.write_settings(i2c)
     }
 
     /// Soft reset chip
+
     pub fn reset_sensor(&self, i2c: &mut hal_I2c) {
         self.write_register(i2c, RESET, RESET_CODE)
     }
--- src/spi.rs
+++ src/spi.rs
@@ -5,6 +5,7 @@ use crate::*;
 /// Struct stores calibration data and settings from the sensor
 ///
 /// Have owns over SS
+
 pub struct BME280<T: OutputPin> {
     ss: T,
     calibrationdata: CalibrationData,
@@ -12,6 +13,7 @@ pub struct BME280<T: OutputPin> {
 }
 
 impl<T: OutputPin> BME280<T> {
+
     /// Function gets tuple with spi bus and SS pin
     ///
     /// Then get calibration data and store settings in sensor
@@ -19,6 +21,7 @@ impl<T: OutputPin> BME280<T> {
     /// ```rust
     /// let mut bme280 = BME280::init(&mut spi, ss); // specify your spi bus and SS pin
     /// ```
+
     pub fn init(combined: (&mut hal_Spi, T)) -> Self {
         let (mut spi, mut ss) = combined;
         let pt_data = Self::read_calibration_data_pt(&mut spi, &mut ss);
@@ -41,6 +44,7 @@ impl<T: OutputPin> BME280<T> {
     /// ```rust
     /// let measure = bme280.get_measures(&mut spi);
     /// ```
+
     pub fn get_measures(&mut self, spi: &mut hal_Spi) -> Measure {
         match self.settings.ctrl_meas & 0b00000011 {
             0b00000000 => (),
@@ -53,46 +57,46 @@ impl<T: OutputPin> BME280<T> {
     fn read_data(&mut self, spi: &mut hal_Spi) -> [u8; BME280_P_T_H_DATA_LEN] {
         let mut data: [u8; BME280_P_T_H_DATA_LEN] = [0; BME280_P_T_H_DATA_LEN];
 
-        self.ss.set_low();
+        let _ = self.ss.set_low();
 
         spi.write(&[BME280_DATA_ADDR | 0x80]).unwrap();
         spi.transfer(&mut data).unwrap();
 
-        self.ss.set_high();
+        let _ = self.ss.set_high();
         data
     }
 
     fn read_calibration_data_pt(spi: &mut hal_Spi, ss: &mut T) -> [u8; BME280_P_T_CALIB_DATA_LEN] {
         let mut data: [u8; BME280_P_T_CALIB_DATA_LEN] = [0; BME280_P_T_CALIB_DATA_LEN];
-        ss.set_low();
+        let _ = ss.set_low();
 
         spi.write(&[BME280_P_T_CALIB_DATA_ADDR | 0x80]).unwrap();
         spi.transfer(&mut data).unwrap();
 
-        ss.set_high();
+        let _ = ss.set_high();
         data
     }
 
     fn read_calibration_data_h(spi: &mut hal_Spi, ss: &mut T) -> [u8; BME280_H_CALIB_DATA_LEN] {
         let mut data: [u8; BME280_H_CALIB_DATA_LEN] = [0; BME280_H_CALIB_DATA_LEN];
 
-        ss.set_low();
+        let _ = ss.set_low();
 
         spi.write(&[BME280_H_CALIB_DATA_ADDR | 0x80]).unwrap();
         spi.transfer(&mut data).unwrap();
 
-        ss.set_high();
+        let _ = ss.set_high();
         data
     }
 
     fn write_register(&mut self, spi: &mut hal_Spi, register: u8, data: u8) {
 
         let data = [register & 0x7f, data];
-        self.ss.set_low();
+        let _ = self.ss.set_low();
 
         spi.write(&data).unwrap();
 
-        self.ss.set_high();
+        let _ = self.ss.set_high();
     }
 
     #[inline]
@@ -105,21 +109,23 @@ impl<T: OutputPin> BME280<T> {
     /// Set you own settings for the sensor
     ///
     /// Read the datasheet if you want to do
+
     pub fn update_settings(&mut self, spi: &mut hal_Spi, settings: Settings) {
         self.settings = settings;
         self.write_settings(spi)
     }
 
     /// Soft reset chip
+
     pub fn reset_sensor(&mut self, spi: &mut hal_Spi) {
         self.write_register(spi, RESET, RESET_CODE)
     }
 
     fn trig_forced_measure(&mut self, spi: &mut hal_Spi) {
         let data = [CTRL_MEAS & 0x7f, self.settings.ctrl_meas];
-        self.ss.set_low();
+        let _ = self.ss.set_low();
         spi.write(&data).unwrap();
-        self.ss.set_high();
+        let _ = self.ss.set_high();
     }
 }
 
